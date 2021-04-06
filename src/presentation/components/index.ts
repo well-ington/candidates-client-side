7f59c35f0c3c336ad528db273e0bb12c22ef613f
Well-ington <celso.w.dias.a@gmail.com>
2021-04-06 04:59:43 -0300

    refactor: added/updated index for global components folder

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,6 @@
+export { Router } from './router/router';
+export { App } from './app/app';
+export { Button } from './button/button';
+export { FilterButton } from './filter-button/filter-button';
+export { default as SearchBar} from './search-bar/search-bar';
+export { SearchSuggestion } from './search-suggestion/search-suggestion';
