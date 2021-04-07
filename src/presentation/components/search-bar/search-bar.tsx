97a5b51013eec711989b6ca65d647f1220a702eb
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 06:56:42 -0300

    feature: updated search bar style, added search icon

--- src/presentation/components/search-bar/search-bar.tsx
+++ src/presentation/components/search-bar/search-bar.tsx
@@ -5,6 +5,7 @@ import { TremoteSuggestions } from '@/data/store/reducer/store';
 import { connect } from 'react-redux';
 import { Button, FilterButton, SearchSuggestion } from '..';
 import styles from './search-bar.scss';
+import searchImage from '../../pages/home/icons/search.png';
 
 interface ISearchBar {
     options?: TremoteSuggestions;
@@ -65,36 +66,35 @@ const SearchBar: React.FC<ISearchBar> = ({options, setQuery, requestSuggestions,
     const selectorInputName = {
         city: <>Qual <span className={styles.highlight}>cidade</span>?</>,
         experience: <>Quanto tempo de <span className={styles.highlight}>experiência</span>?</>,
-        technologies: <>Quais <span className={styles.highlight}>technologias</span>?</>
+        technologies: <>Quais <span className={styles.highlight}>tecnologias</span>?</>
     }
 
     const isValidQuery = filterRequest.city.length && filterRequest.experience.length && filterRequest.technologies.length;
 
 
 
-    return <div className={styles.container}>        
-
-            
-            
-            <Button disabled={!isValidQuery} variant="proceed" action={() => {
-                setValue('');
-                setQuery();
-                setLoading();
-                setTimeout(() => {
-                    initializeQuery(filterRequest);
-                }, 500);
-            }}>
-                Pesquisar
-            </Button>
-
+    return <div className={styles.container}>
+        <div className={styles.infoWrapper}>
             <p className={styles.inputDisplay}>{selectorInputName[lazyKeySelector]}</p>
-            <input className={styles.textInput} data-testid="searchBar" value={value} type="text" onChange={(e: {target: {value: string}}) => setValue(e.target.value)} />
-            
-            
+            <div className={styles.inputButtonWrapper}>
+                <input className={styles.textInput} data-testid="searchBar" value={value} type="text" onChange={(e: {target: {value: string}}) => setValue(e.target.value)} />
+                <div className={styles.buttonWrapper}>
+                <Button disabled={!isValidQuery} variant="proceed" action={() => {
+                    setValue('');
+                    setQuery();
+                    setLoading();
+                    setTimeout(() => {
+                        initializeQuery(filterRequest);
+                    }, 500);
+                }}>
+                    <img className={styles.searchIcon} src={searchImage} />
+                </Button>
 
-        <SearchSuggestion value={value} change={changeValue} options={options[lazyKeySelector] || []} />
-        <FilterButton filter={filterRequest} setFilter={setFilterRequest} />
-        
+                </div>
+            </div>            
+            <SearchSuggestion value={value} change={changeValue} options={options[lazyKeySelector] || []} />
+            <FilterButton filter={filterRequest} setFilter={setFilterRequest} />
+        </div>
     </div>
 }
 
