2b3bb4b42255bd4fcdb5e3d929ec0e8ba334c0ce
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 07:00:37 -0300

    refactor: changed Candidate Display component style and added media query to handle smaller devices

--- src/presentation/pages/home/components/candidate-display/candidate-display.tsx
+++ src/presentation/pages/home/components/candidate-display/candidate-display.tsx
@@ -4,31 +4,28 @@ import styles from './candidate-display.scss';
 
 type TresultContainer = {
     candidate: TcandidateObject;
-    card: boolean;
+    card?: boolean;
 }
 
-const CandidateDisplay: React.FC<TresultContainer> = ({ card, candidate: { id, experience, mainTech, tech, city }}) => {
-    return <div className={`${styles.container} ${card ? styles.card : styles.row}`}>
-        <div>
-            <div className={styles.title}>Candidate {id}</div>
-            <div className={styles.highlight}>Experiência: {experience}</div>
-        </div>
-        <div>
-            <div className={styles.title}>
-            Manja mais de:
+const CandidateDisplay: React.FC<TresultContainer> = ({ candidate: { id, matched, experience, mainTech, tech, city, techScore, mainTechScore }}) => {
+
+      const diff = Number(experience) - Math.floor(Number(experience));
+        const stringExperience = diff === 0.5 ? `de ${Math.floor(Number(experience))} a ${Math.floor(Number(experience)) + 1} anos`
+        : diff === 0.9 ? "mais de " + Math.floor(Number(experience)) : Math.floor(Number(experience)) + " anos";
+      
+      return <div className={styles.container}>
+        <div className={styles.firstRow}>
+            <div className={styles.infoBlock}>
+                <p className={styles.title}>Candidato {id}</p>
+                <p className={styles.sub}>{city}</p>
+                <p className={styles.sub}>Experiência: <span className={styles.highlight}>{stringExperience}</span></p>
             </div>
-            <div className={styles.highlight}>
-            {mainTech.join(', ')}
+            <div className={styles.techs}>                
+                {
+                    tech.map((item, index) => <p className={`${styles.techItem} ${matched.indexOf(item) !== -1 ? styles.matchedItem : ''}`} key={`tech_item_${index}`}>{item}</p>)
+                }
             </div>
-        </div>
-        <div>
-            <div className={styles.title}>
-            Tecnologias:
-            </div>
-            <div>
-            {tech.map(item => <span className={mainTech.indexOf(item) !== -1 ? styles.highlight :  styles.sub} key={`key-${id}-${item}`}>{item}</span>)}
-            </div>
-        </div>
+        </div> 
     </div>
 }
 
