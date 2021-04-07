14ef993b63810e26076a62d122c00601ff75eec7
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 06:52:38 -0300

    feature: added request error message and zero results message

--- src/data/store/actions/actions.ts
+++ src/data/store/actions/actions.ts
@@ -1,9 +1,9 @@
-import { GetServerData } from '@/data/request/request';
-import { Dispatch } from 'redux';
-import { GET_SUGGESTIONS, GET_RESULTS, UPDATE_FILTER, SET_LOADING, UNSET_LOADING, REQUEST_ERROR } from './action-types';
-import axios from 'axios';
+import { GetServerData } from "@/data/request/request";
+import { Dispatch } from "redux";
+import { GET_SUGGESTIONS, GET_RESULTS, UPDATE_FILTER, SET_LOADING, UNSET_LOADING, REQUEST_ERROR, CLEAR_ERROR } from "./action-types";
+import axios from "axios";
 
-const dataHandler = new GetServerData('http://localhost:5050', axios);
+const dataHandler = new GetServerData("https://candidates-server-side.herokuapp.com", axios);
 
 export const getServerSuggestions = () => {
     const returnedDispatchFunction = async (dispatch: Dispatch) => {
@@ -27,28 +27,44 @@ export const getServerSuggestions = () => {
 }
 
 export const getServerQuery = (query: any) => {
-    return async (dispatch: Dispatch) => {        
-        try {
-
+    return async (dispatch: Dispatch) => { 
+        dispatch({
+            type: CLEAR_ERROR
+        });       
+        try { 
             const data: any = await dataHandler.getResults(query);
-            const stringedQuery = JSON.stringify(query);            
+            const stringedQuery = JSON.stringify(query); 
             
-            dispatch({
-                type: GET_RESULTS,
-                payload: {
-                    date: Date.now(),
-                    query: stringedQuery,
-                    candidates: data.results
-                }
-            });
+            if (data.results.length === 0) {
+                dispatch({
+                    type: REQUEST_ERROR,
+                    payload: "no results"
+                });
+
+            } else {
+                dispatch({
+                    type: GET_RESULTS,
+                    payload: {
+                        date: Date.now(),
+                        query: stringedQuery,
+                        candidates: data.results
+                    }
+                });
+            }
+            
+            
             dispatch({
                 type: UNSET_LOADING
             });
         } catch (error) {
-            // console.log('ops, request error');
             dispatch({
-                type: REQUEST_ERROR
+                type: UNSET_LOADING
             });            
+            
+            dispatch({
+                type: REQUEST_ERROR,
+                payload: "something went wrong"
+            });         
         }
     }
 }
