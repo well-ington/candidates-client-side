777c72bde8036d36ab953b7c3fa46bd9a8abe6aa
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 06:54:56 -0300

    test: added button test

--- src/presentation/components/button/button.spec.tsx
+++ src/presentation/components/button/button.spec.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "./button";
-
 import { fireEvent, render, RenderResult } from "@testing-library/react";
 
 class buttonSpy {
@@ -17,18 +16,14 @@ const makeSut = (innerText: string = "button", disabled: boolean = false): {
     sut: RenderResult,
     spy: buttonSpy,
     button: HTMLButtonElement
-} => {   
-
+} => {
     const testid: string = "test-button";
-
     const spy = new buttonSpy();
-
     const sut = render(<div>
         <Button testid={testid} disabled={disabled} variant="proceed" action={() => spy.triggerButton()}>{innerText}</Button>
         </div>);
     
     const button = sut.getByTestId(testid) as HTMLButtonElement;
-
     return {
         sut,
         spy,
@@ -38,20 +33,24 @@ const makeSut = (innerText: string = "button", disabled: boolean = false): {
 
 describe("Testing button basic functionalities", () => {
     it("Renders correctly", () => {
-        const { button } = makeSut();
+        const buttonText = 'BUTTON';
+        const { button } = makeSut(buttonText);
         expect(button).toBeTruthy();
+        expect(button.innerHTML).toEqual(buttonText);
     });
 
     it("Triggers the target action button", () => {
         const { button, spy } = makeSut();
         fireEvent.click(button);
         expect(spy.triggered).toEqual(true);
+        expect(button.disabled).toEqual(false);
     });
 
     it("Triggers the target action button", () => {
         const { button, spy } = makeSut("button", true);
         fireEvent.click(button);
         expect(spy.triggered).toEqual(false);
+        expect(button.disabled).toEqual(true);
     });
     
 });
--- src/presentation/components/button/button.tsx
+++ src/presentation/components/button/button.tsx
@@ -6,10 +6,11 @@ type TProps = {
     action: () => void;
     disabled?: boolean;
     testid?: string;
+    className?: string;
 }
 
-export const Button: React.FC<TProps> = ({action, variant, children, disabled = false, testid = 'generic-button'}) => {
-    return <button data-testid={testid} disabled={disabled} className={`${styles.button} ${styles[variant]} ${disabled ? styles.inactive : styles.active}`} onClick={action}>
+export const Button: React.FC<TProps> = ({action, variant, children, disabled = false, testid = 'generic-button', className}) => {
+    return <button data-testid={testid} disabled={disabled} className={`${styles.button} ${styles[variant]} ${disabled ? styles.inactive : styles.active} ${className ? className : ''}`} onClick={action}>
         {children}
     </button>
 }
