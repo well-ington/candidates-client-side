93f9204565dd95861b231bb571db665ec8f12795
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 06:55:55 -0300

    feature: added Icon Attribution component

---
+++ src/presentation/components/icon-attribution/icon-attribution.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './icon-attribution.scss';
+
+
+export const IconAttribution: React.FC = () => {
+    return <div className={styles.container}>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+}
