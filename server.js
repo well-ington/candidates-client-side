4ecb5b17010616037bc8d20b3bb70017ea120721
Well-ington <celso.w.dias.a@gmail.com>
2021-04-07 14:34:43 -0300

    fix: added redirect to other routes

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const app = express();
  
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
+
+app.get('*', (req, res) => {
+    res.redirect('/')
+});
  
 app.listen(app.get('port'));
